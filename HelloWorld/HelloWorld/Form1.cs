1af1ec6a6156f635c966d37a130f577e4a61a918
Nanapon2002 <64030090@kmitl.ac.th>
2022-11-21 20:04:29 +0700

    Update User Interface

--- HelloWorld/HelloWorld/Form1.cs
+++ HelloWorld/HelloWorld/Form1.cs
@@ -16,5 +16,15 @@ namespace HelloWorld
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
