df10272e00ba7d52c3f591777e88c4d00a0fead7
Nanapon2002 <64030090@kmitl.ac.th>
2022-11-21 20:38:01 +0700

    updete user

--- HelloWorld/HelloWorld/Form1.Designer.cs
+++ HelloWorld/HelloWorld/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,12 +43,25 @@
             this.label1.Text = "Branch: main";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(282, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 94);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Form design";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SandyBrown;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,6 +73,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- HelloWorld/HelloWorld/Form1.cs
+++ HelloWorld/HelloWorld/Form1.cs
@@ -26,5 +26,10 @@ namespace HelloWorld
         {
 
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
